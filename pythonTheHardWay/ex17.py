c79b1911e33a52c64a7e2e2552c1300743049bf2
Nragis <Quinn_murphey@me.com>
2020-03-11 04:27:30 -0500

    Finished PtHW exercise 17

---
+++ pythonTheHardWay/ex17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, fromFile, toFile = argv
+
+print(f"Copying from {fromFile} to {toFile}")
+
+inFile = open(fromFile)
+inData = inFile.read()
+
+print(f"The input file is {len(inData)} bytes long")
+
+print("Does the output file exist? {exists(toFile)}")
+print("Ready? Hit RETURN to continue, CRTL-C to abort")
+input()
+
+outFile = open(toFile, "w")
+outFile.write(inData)
+
+print("Alright, all done")
+
+outFile.close()
+inFile.close()
