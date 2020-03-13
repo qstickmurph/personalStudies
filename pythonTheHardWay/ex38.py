b0bd81d5443c2e92cb31f96702ad6ca255c8b6a6
Nragis <Quinn_murphey@me.com>
2020-03-13 02:09:49 -0500

    Finished PtHW exercise 38

---
+++ pythonTheHardWay/ex38.py
@@ -0,0 +1,24 @@
+tenThings = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Let's fix that.")
+
+stuff = tenThings.split(' ')
+moreStuff = ["Day", "Night", "Song", "Frisbee",
+            "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    nextOne = moreStuff.pop()
+    print("Adding: ", nextOne)
+    stuff.append(nextOne)
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(' '.join(stuff))
+print("#".join(stuff[3:5]))
+
