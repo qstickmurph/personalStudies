642c9e9c39f143c39684933c530ec7517091d12a
Nragis <Quinn_murphey@me.com>
2020-04-20 10:12:45 -0500

    Added LoginManager to app, also attatcher LoginManager to app.models.User

--- learnFlask/app/__init__.py
+++ learnFlask/app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import loginManager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login = loginManager(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
