0dca7e2a2653d14fc8476cbaf9755c29a3736de1
Nragis <Quinn_murphey@me.com>
2020-04-20 12:18:07 -0500

    Added a resgister form, register.html, and connected it to the database

--- learnFlask/app/routes.py
+++ learnFlask/app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask_login import current_user, login_user, login_required
+from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db, login
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route("/")
@@ -20,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template(url_for('index'), title = "Home", posts=posts)
+    return render_template('index.html', title = "Home", posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -43,3 +43,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations! You are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
