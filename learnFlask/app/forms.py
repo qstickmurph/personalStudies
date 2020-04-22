a49a3406273809d8d8426c37729f0d92510934fc
Nragis <Quinn_murphey@me.com>
2020-04-22 13:06:51 -0500

    Added an edit profile page and about_me and last_seen

--- learnFlask/app/forms.py
+++ learnFlask/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -26,4 +26,7 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('There exists an account with that email address. Please use another one')
 
-    
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
