b24e68fd0cc54dc591dc4467194669d121b91722
Nragis <Quinn_murphey@me.com>
2020-04-25 20:57:58 -0500

    Added post functionality, showing followed posts on index, and an explore page

--- learnFlask/app/forms.py
+++ learnFlask/app/forms.py
@@ -40,3 +40,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say Something', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
