960b8c0e62da71752461d3e197c570f238a90c2d
Nragis <Quinn_murphey@me.com>
2020-04-28 10:46:49 -0500

    Changed all html templates to incorperate bootstrap templates

--- learnFlask/app/email.py
+++ learnFlask/app/email.py
@@ -1,8 +1,22 @@
 from flask_mail import Message
 from app import mail
+from threading import Thread
+
+def send_async_email(app, message):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
     mail.send(msg)
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog] Reset your password',
+        sender=app.config['ADMINS'][0],
+        recipients=[user.email],
+        text_body=render_template('email/reset_password.txt', user=user, token=token),
+        html_body=render_template('email/reset_password.html', user=user, token=token))
--- learnFlask/app/routes.py
+++ learnFlask/app/routes.py
@@ -71,7 +71,7 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page',1,type=int)
-    posts = user.posts.order_by(Posts.timestamp.desc()).paginate(page,app.config['POSTS_PER_PAGE'], False)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page,app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('user', username=username, page=posts.next_num) if posts.has_next else None
     prev_url = url_for('user', username=username, page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
@@ -143,3 +143,18 @@ def reset_password_request():
         flash('Check your email for the instructions to reset your password')
         return redirect(url_for('login'))
     return render_template('reset_password_request.html', title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET','POST'])
+def reset_password(token):
+    if curent_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
