58bbf9fbbec8331f973f4506ec6a34a4113c441b
Nragis <Quinn_murphey@me.com>
2020-04-30 15:24:20 -0500

    Finished chapter 1

--- SICP/chapter1/exercise_1-37.scm
+++ SICP/chapter1/exercise_1-37.scm
@@ -1,6 +1,23 @@
+(define (cont-frac-iter n d k val)
+  (if (= k 0) 
+	  val
+	  (cont-frac-iter n d (- k 1) (/ (n k) (+ (d k) val)))))
+
+(define (cont-frac-recur n d k max)
+  (if (= k max)
+	  0
+	  (/ (n k) (+ (d k) (cont-frac-recur n d (+ k 1) max)))))
+
 (define (cont-frac n d k)
-  (cont-frac-recur n d k 0))
+  (cont-frac-iter n d k 0))
 
-(define (cont-frac-recur n d max k)
-  ())
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           100)
 
+(define (cont-frac n d k)
+  (cont-frac-recur n d 1 k))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           100)
