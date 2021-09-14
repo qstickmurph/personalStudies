771f6ea912f41b69a20b24612ca3b86479959599
Nragis <Quinn_murphey@me.com>
2021-09-13 19:09:25 -0500

    Finished hw1 for CS3583

--- csclasses/3853/a1/ex2.c
+++ csclasses/3853/a1/ex2.c
@@ -1,42 +1,74 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define NUM_BITS 16
+#define NUM_BITS 4
 
-typedef struct int_ll{ // Custom LL for holding ints in a dynamically allocated fashion
-    int value;
-    struct int_ll *next;
-} int_ll;
-
-void printAsBinary(int in);
+void readBinaryInput(int bin[]);
+void printBinary(int bin[]);
+void convertToBinary(int dec, int bin[]);
+int convertToDecimal(int bin[]);
+int add(int num1[], int num2[], int res[]);
 
 int main(int argc, char *argv[]){
-    int_ll *llHead = NULL;
-    int_ll *llRoot = NULL;
+    char input;
+    int num1Dec;
+    int num2Dec;
 
-    int temp;
-    while( (scanf("%d", &temp)) ){ // Loop until the user enters a 0
-        if(temp == 0){
-            break;
+    int num1Bin[NUM_BITS];
+    int num2Bin[NUM_BITS];
+    int result[NUM_BITS];
+
+    printf("Would you like your inputs to be in (a) binary or (b) decimal?\n");
+    scanf(" %c", &input);
+    if( input == 'a' ){
+        printf("num1 (bin) = ");
+        readBinaryInput(num1Bin);
+        num1Dec = convertToDecimal(num1Bin);
+        printf("num1 (dec) = %d\n\n", num1Dec);
+
+        printf("num2 (bin) = ");
+        readBinaryInput(num2Bin);
+        num2Dec = convertToDecimal(num2Bin);
+        printf("num2 (dec) = %d\n\n", num2Dec);
+
+    }else {
+        printf("num1 (dec) = ");
+        scanf("%d", &num1Dec);
+        if( num1Dec >= 1 << (NUM_BITS - 1) || num1Dec < -(1 << (NUM_BITS -1)) ){
+            printf("ERR: Input number too large. Please enter only numbers between %d and %d\n", (1 << (NUM_BITS - 1)) - 1, -(1 << (NUM_BITS - 1)));
+            return 1;
+        }else{
+            convertToBinary(num1Dec, num1Bin);
+            printf("num1 (bin) = ");
+            printBinary(num1Bin);
+            printf("\n");
         }
 
-        int_ll *temp_ll = malloc(sizeof(int_ll)); // Create new linked list entry
-        temp_ll->value = temp;
-
-        if(llHead == NULL){ // Initialize ll if uninitialized
-            llHead = temp_ll;
-            llRoot = temp_ll;
-        }else{ // Otherwise, append entry
-            llRoot->next = temp_ll;
-            llRoot = temp_ll;
+        printf("num2 (dec) = ");
+        scanf("%d", &num2Dec);
+        if( num2Dec >= 1 << (NUM_BITS - 1) || num2Dec < -(1 << (NUM_BITS -1)) ){
+            printf("ERR: Input number too large. Please enter only numbers between %d and %d\n", (1 << (NUM_BITS - 1)) - 1, -(1 << (NUM_BITS - 1)));
+            return 1;
+        }else{
+            convertToBinary(num2Dec, num2Bin);
+            printf("num2 (bin) = ");
+            printBinary(num2Bin);
+            printf("\n");
         }
     }
 
-    for(int_ll *curr = llHead; curr != NULL; ){ // Loop over ll and run printAsBinary on each value
-        printAsBinary(curr->value);
-        int_ll *temp = curr;
-        curr = curr->next;
-        free(temp);
+    int overflow = add(num1Bin, num2Bin, result);
+
+    printf("res  (bin) = ");
+    printBinary(result);
+    printf("res  (dec) = %d\n", convertToDecimal(result));
+    printf("\n");
+    
+    printf("Overflowed? : ");
+    if( overflow == 0 ){
+        printf("No\n");
+    }else{
+        printf("Yes\n");
     }
 
     return 0;
@@ -50,39 +82,89 @@ void flipBit(int *n){ // Self explanatory: 1 -> 0 and 0 -> 1
     }
 }
 
-void printAsBinary(int in){
-    if( in >= 1 << (NUM_BITS - 1) || in < -(1 << (NUM_BITS -1)) ){ // Test if number can be represented as an NUM_BITS bit number
-        printf("ERR: Input number too large. Please enter only numbers between %d and %d\n", (1 << (NUM_BITS - 1)) - 1, -(1 << (NUM_BITS - 1)));
-        return;
+void readBinaryInput(int bin[]){
+    char input;
+    for(int i = 0; i < NUM_BITS; i++){
+        scanf(" %c", &input);
+        if(input == '0'){
+            bin[i] = 0;
+        }else if(input == '1'){
+            bin[i] = 1;
+        }else{
+            printf("ERR: Only enter a 0s and 1s in binary entry mode\n");
+            exit(1);
+        }
     }
-    int outputBits[NUM_BITS];
+}
 
-    if(in > 0){ // Initialize outputBits to 0s when positive
-        for(int i = 0; i < NUM_BITS; i++){
-            outputBits[i] = 0;
-        }
-    }else{ // Initialize outputBits to 1s when positive
-        for(int i = 0; i < NUM_BITS; i++){
-            outputBits[i] = 1;
-        }
-        // Then map in onto its positive complement 
-        in = -in - 1;
-    }
-    
-    int index;
-    for(index = 1; index < NUM_BITS; index++){
-        int value = 1 << (NUM_BITS - 1 - index); // 2 ^ (NUM_BITS - 1 - index)
-        if( in >= value ){ // Check each power of two decreasing
-            in -= value;
-            flipBit( outputBits + index ); // Flip the bit from 0 to 1 or vice versa. This allows for the usage of the same loop for both positive and negative
-        }
-    }
-    
-    for(int i = 0; i < NUM_BITS; i++){ // Print out the outputBits array formatted
-        printf("%d", outputBits[i]);
+void printBinary(int bin[]){
+    for(int i = 0; i < NUM_BITS; i++){
+        printf("%d", bin[i]);
         if( (i % 4) == 3 ){
             printf(" ");
         }
     }
     printf("\n");
+}
+
+void convertToBinary(int dec, int bin[]){
+    if(dec > 0){ // Initialize outputBits to 0s when positive
+        for(int i = 0; i < NUM_BITS; i++){
+            bin[i] = 0;
+        }
+    }else{ // Initialize outputBits to 1s when positive
+        for(int i = 0; i < NUM_BITS; i++){
+            bin[i] = 1;
+        }
+        // Then map in onto its positive complement 
+        dec = -dec - 1;
+    }
+    
+    int index;
+    for(index = 1; index < NUM_BITS; index++){
+        int value = 1 << (NUM_BITS - 1 - index); // 2 ^ (NUM_BITS - 1 - index)
+        if( dec >= value ){ // Check each power of two decreasing
+            dec -= value;
+            flipBit( bin + index ); // Flip the bit from 0 to 1 or vice versa. This allows for the usage of the same loop for both positive and negative
+        }
+    }
+
+    
+}
+
+int convertToDecimal(int bin[]){
+    int sum;
+    if(bin[0] == 1){
+        sum = -(1 << (NUM_BITS - 1));
+    }else{
+        sum = 0;
+    }
+
+    for(int i = 1; i < NUM_BITS; i++){
+        if( bin[i] == 1 ){
+            sum += 1 << (NUM_BITS - 1 - i);
+        }
+    }
+
+    return sum;
+}
+
+int add(int num1[], int num2[], int res[]){
+    int carry = 0;
+    for(int i = NUM_BITS - 1; i >= 0; i--){
+        res[i] = num1[i] ^ num2[i];
+        res[i] = res[i] ^ carry;
+        if( num1[i] + num2[i] + carry > 1 ){
+            carry = 1;
+        }else{
+            carry = 0;
+        }
+    }
+    if( (num1[0] == 0) && (num2[0] == 0) && (res[0] == 1) ){
+        return 1;
+    }else if( (num1[0] == 1) && (num2[0] == 1) && (res[0] == 0) ){
+        return 1;
+    }else{
+        return 0;
+    }
 }
